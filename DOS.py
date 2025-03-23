93f87ebe2d669e651f8c237f00af2ff4b8ca8eae
rosh105 <rtrivedi9@gatech.edu>
2025-03-23 16:49:59 -0400

    Worked on DOS, need to include integral for charge and bring together with potential

--- DOS.py
+++ DOS.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+E_0 = 1 #Conduction band energy
+psi = np.zeros((100,100))#Placeholder for potential values
+E_c = E_0 - psi
 def tail_DOS(E):
     g_0 = 1 #Placeholder value
-    E_c = 1 #Placeholder value
     sigma = 1 #Placeholder value
     return g_0*np.exp((E-E_c)/sigma)
 def gauss_DOS(E):
     g_0 = 1 #Placeholder value
-    E_c = 1 #Placeholder value
     sigma = 1 #Placeholder value
     return g_0*np.exp(-(E-E_c)**2/2*sigma**2)
 
-print(tail_DOS(-0.1))
-print(gauss_DOS(-0.1))
